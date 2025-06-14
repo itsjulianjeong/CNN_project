ea946ce5a577fd50ac2f9fa2d9c32972f3b2a1a3
Julian Jeong <its.julianjeong@gmail.com>
2025-06-14 23:47:17 +0900

    ✨ 이미지 전처리 및 시각화 유틸 함수 완성
    - image_utils.py: to_grayscale, resize_image, normalize_image 함수 구현
    - draw_result.py: 예측값을 화면에 시각적으로 출력하는 draw_prediction 함수 구현
    - 코드 내 처리 흐름에 맞는 이미지 전처리 파이프라인 정립

--- webcam_app/utils/image_utils.py
+++ webcam_app/utils/image_utils.py
@@ -1,1 +1,16 @@
 # 이미지 전처리 함수 통합 (grayscale, 이미지 크기 조정, 픽셀값 0~1 사이로 정규화)
+
+import cv2
+import numpy as np
+
+def to_grayscale(image):
+    # BGR -> 흑백 1채널로 변환
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)[..., np.newaxis]  # (H, W, 1)
+
+def resize_image(image, size=(86, 86)):
+    # 모델 입력 크기로 리사이즈 86x86
+    return cv2.resize(image, size).reshape(size[1], size[0], 1)  # (H, W, 1)
+
+def normalize_image(image):
+    # 픽셀 값을 0~1 범위로 정규화
+    return image.astype("float32") / 255.0
